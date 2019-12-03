45d7e82e4abb94b3b1076f37a93736afdc9186a1
marceloadf <marceloferreira.eng@hotmail.com>
2019-12-03 20:24:11 -0300

    Adicionado pipes na tela de lançamento

--- src/app/lancamento-pesquisa/lancamento-pesquisa.component.ts
+++ src/app/lancamento-pesquisa/lancamento-pesquisa.component.ts
@@ -8,56 +8,56 @@ import { Component, OnInit } from '@angular/core';
 export class LancamentoPesquisaComponent {
   lancamentos = [
     {
-      tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
+      tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2019, 0, 19),
       dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'
     },
     {
-      tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/06/2017',
-      dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil'
+      tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: new Date(2017, 6, 10),
+      dataPagamento: new Date(2017, 5, 30), valor: 80000, pessoa: 'Atacado Brasil'
     },
     {
-      tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: '20/07/2017',
+      tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: new Date(2017, 7, 20),
       dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda'
     },
     {
-      tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: '05/06/2017',
-      dataPagamento: '30/05/2017', valor: 800, pessoa: 'Escola Abelha Rainha'
+      tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: new Date(2017, 5, 6),
+      dataPagamento: new Date(2017, 6, 30), valor: 800, pessoa: 'Escola Abelha Rainha'
     },
     {
-      tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: '18/08/2017',
+      tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: new Date(2017, 2, 20),
       dataPagamento: null, valor: 55000, pessoa: 'Sebastião Souza'
     },
     {
-      tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: '10/07/2017',
-      dataPagamento: '09/07/2017', valor: 1750, pessoa: 'Casa Nova Imóveis'
+      tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: new Date(2017, 10, 7),
+      dataPagamento: new Date(2017, 9, 7), valor: 1750, pessoa: 'Casa Nova Imóveis'
     },
     {
-      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/07/2017',
+      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(2017, 12, 25),
       dataPagamento: null, valor: 180, pessoa: 'Academia Top'
     }
     ,
     {
-      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/07/2017',
+      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(2017, 12, 25),
       dataPagamento: null, valor: 180, pessoa: 'Academia Top'
     }
     ,
     {
-      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/07/2017',
+      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(2017, 12, 25),
       dataPagamento: null, valor: 180, pessoa: 'Academia Top'
     }
     ,
     {
-      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/07/2017',
+      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(2017, 12, 25),
       dataPagamento: null, valor: 180, pessoa: 'Academia Top'
     }
     ,
     {
-      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/07/2017',
+      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(2017, 12, 25),
       dataPagamento: null, valor: 180, pessoa: 'Academia Top'
     }
     ,
     {
-      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/07/2017',
+      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(2017, 12, 25),
       dataPagamento: null, valor: 180, pessoa: 'Academia Top'
     }
   ];
